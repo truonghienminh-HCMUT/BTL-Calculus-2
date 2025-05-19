38113ffa90ac9ea3e65a92b6c82f4ade60618230
Trần Ngọc Phương Mai <mai.tranngocphuongmai2452720@hcmut.edu.vn>
2025-05-20 01:19:30 +0700

    ahuhu

--- manim/Official/scene16/scene16.py
+++ manim/Official/scene16/scene16.py
@@ -49,12 +49,21 @@ class ChiaNho(MovingCameraScene):
         Text0.shift((DOWN * 2.5), LEFT * 5.2)
         Text01 = Tex("O").to_edge(UP * 7,  buff=0.5).scale(1)
         Text01.shift((DOWN * 2.5), RIGHT * 0.9)
+        rb = Tex("r = b").scale(1).next_to(axes1.c2p(a, 0), UP)
+        rb.move_to([-1.6, 0.8, 0])
+        ra = Tex("r = a").scale(0.7).next_to(axes2.c2p(b, 0), UP)
+        ra.move_to([-3.9, -1, 0])
 
         outer_arc = Arc(radius=b, start_angle=alpha, angle=beta - alpha, arc_center=origin)
         inner_arc = Arc(radius=a, start_angle=beta, angle=-(beta - alpha), arc_center=origin)
 
-        left_line = Line(origin, outer_arc.point_from_proportion(0))
-        right_line = Line(origin, outer_arc.point_from_proportion(1))
+        # Tính điểm đầu dựa trên góc alpha
+        left_point = origin + b * np.array([np.cos(alpha), np.sin(alpha), 0])
+        right_point = origin + b * np.array([np.cos(beta), np.sin(beta), 0])
+
+        # Tạo các đoạn thẳng từ origin
+        left_line = Line(origin, left_point)
+        right_line = Line(origin, right_point)
 
         outer_points = [b * np.array([np.cos(t), np.sin(t), 0]) for t in np.linspace(alpha, beta, 30)]
         inner_points = [a * np.array([np.cos(t), np.sin(t), 0]) for t in np.linspace(beta, alpha, 30)]
@@ -68,14 +77,24 @@ class ChiaNho(MovingCameraScene):
         # Tạo vùng D tô màu
         region = VGroup(outer_arc, right_line, inner_arc, left_line).set_fill(RED, opacity=0.5).set_stroke(BLUE)
 
-        part = VGroup(outer_arc, inner_arc, left_line, right_line, region)
-        part.shift(LEFT * 0)
+        x_axis_point = axes1.c2p(1, 0)
+        x_axis_line_1 = Line(origin, x_axis_point, color=RED)
+        angle = Angle(
+            x_axis_line_1, left_line,
+            radius=0.9,
+            color=BLUE,
+            quadrant=(1,-1),  # Chọn phần tư
+            other_angle=False,  # Vẽ góc lớn hơn 180°
+            fill_opacity=0.5   # Độ trong suốt
+        )
 
-        
+        part = VGroup(outer_arc, inner_arc, left_line, right_line, region, x_axis_line_1, angle)
+        part.shift(LEFT * 0)
         self.play(Create(axes1), Create(axes2))
         self.play(Create(axes1_labels), Create(axes2_labels), 
                   Write(Text0), Write(Text01))
         self.play(Create(part), run_time=2)
+        self.play(Write(rb), Write(ra), run_time=1)
 
         def update_curve(mob):
             mob.move_to(moving_dot.get_center())
