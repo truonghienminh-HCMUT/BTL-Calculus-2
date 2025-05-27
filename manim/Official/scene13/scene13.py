0d46ac0e88c1d8e4f9ddab75443391e989fdbf03
Trần Ngọc Phương Mai <mai.tranngocphuongmai2452720@hcmut.edu.vn>
2025-05-27 18:07:51 +0700

    scene13

--- manim/Official/scene13/scene13.py
+++ manim/Official/scene13/scene13.py
@@ -17,7 +17,7 @@ config.tex_template = VIETNAMESE_TEMPLATE
 
 config.frame_rate = 60
 
-class ChiaNho(MovingCameraScene):
+class SCENE13(MovingCameraScene):
     def construct(self):
         self.camera.frame.save_state()
         
@@ -28,16 +28,16 @@ class ChiaNho(MovingCameraScene):
         #TEXT CỦA HÌNH MÔ PHỎNG BÊN TRÁI
         TEXT_scene13 = Tex(
             r"\textbf{CÁCH CHUYỂN TỪ TỌA ĐỘ DESCARTES", 
-            font_size=40
-        ).shift(UP * 1)
+            font_size=45
+        ).shift(UP * 0.5)
         TEXT_small_scene13 = Tex(
             r"\textbf{CÁCH CHUYỂN TỪ TỌA ĐỘ DESCARTES", 
             font_size=30
         ).shift(UP * 3 + RIGHT * 3)
         TEXT_1_scene13 = Tex(
             r"\textbf{SANG TỌA ĐỘ CỰC}",#0"
-            font_size=40
-        )
+            font_size=45
+        ).shift(DOWN * 0.5)
         TEXT_small_1_scene13 = Tex(
             r"\textbf{SANG TỌA ĐỘ CỰC}",#0"
             font_size=30
@@ -87,7 +87,7 @@ class ChiaNho(MovingCameraScene):
 
         dashed_line_y_scene13 = DashedLine(start=LEFT*3, end=RIGHT*3, dash_length=0.2)
         dashed_line_y_scene13 = DashedLine(start=[2.197,1.46,0], end=[2.197,-0,0])
-        dashed_line_y_scene13.set_color(RED)
+        dashed_line_y_scene13.set_color(RED) 
         dashed_line_y_scene13.shift(LEFT * 2.5)
 
         dashed_line_y1_scene13 = DashedLine(start=LEFT*3, end=RIGHT*3, dash_length=0.2)
@@ -283,7 +283,6 @@ class ChiaNho(MovingCameraScene):
         Text_7_scene13.set_color(WHITE)
 
         #CHẠY ANIMATION
-        self.play(Transform(TEXT_scene13, TEXT_small_scene13), Transform(TEXT_1_scene13, TEXT_small_1_scene13), run_time=2)
         self.play(Write(Text_1_scene13[0]), Write(Text_1_scene13[1]), run_time=2)
         self.play(Write(Text_1_scene13[2]), Write(Text_1_scene13[3]), Write(Text1_scene13), run_time=2)
         self.play(Write(Text_1_scene13[4]), Write(Text_1_scene13[5]))
@@ -297,10 +296,15 @@ class ChiaNho(MovingCameraScene):
         self.play(Write(Text_2_scene13[0]), Write(Text_2_scene13[1]), Write(Text_2_scene13[2]), Write(Text_2_scene13[3]), Write(Text_2_scene13[4]), 
                 Write(Text_2_scene13[5]), Write(Text_2_scene13[6]), Write(Text_2_scene13[7]), Write(Text_2_scene13[8]),
                  run_time=2)
+        self.wait(1)
+        self.remove(Text_1_scene13, Text_2_scene13, axes_scene13, axes_labels_scene13, x_numbers_scene13, y_numbers_scene13, dot_scene13, Px1y1_scene13, dashed_line_y_scene13, dashed_line_y_scene13,
+                Text_2_scene13[0], Text_2_scene13[1], Text_2_scene13[2], Text_2_scene13[3], Text_2_scene13[4], 
+                Text_2_scene13[5], Text_2_scene13[6], Text_2_scene13[7], Text_2_scene13[8], dashed_line_x_scene13, Text1_scene13)
+        self.play(Write(TEXT_scene13), Write(TEXT_1_scene13), run_time=2)
+        self.wait(1)
+        self.add(Text_1_scene13, axes_scene13, axes_labels_scene13, x_numbers_scene13, y_numbers_scene13, dot_scene13, Px1y1_scene13, dashed_line_y_scene13, dashed_line_y_scene13, dashed_line_x_scene13, Text1_scene13)
+        self.play(Transform(TEXT_scene13, TEXT_small_scene13), Transform(TEXT_1_scene13, TEXT_small_1_scene13), run_time=2)
         self.play(Create(line1_scene13), Create(r_scene13), FadeOut(x_numbers_scene13), FadeOut(y_numbers_scene13), Create(axes1_scene13), Create(x_1_scene13), Create(y_1_scene13) )
-        self.play(Unwrite(Text_2_scene13[0]), Unwrite(Text_2_scene13[1]), Unwrite(Text_2_scene13[2]), Unwrite(Text_2_scene13[3]), Unwrite(Text_2_scene13[4]), 
-                Unwrite(Text_2_scene13[5]), Unwrite(Text_2_scene13[6]), Unwrite(Text_2_scene13[7]), Unwrite(Text_2_scene13[8]),
-                run_time=2)
         self.play(Write(Text_3_scene13[0]), Write(Text_3_scene13[1]), Write(Text_3_scene13[2]), Write(Text_3_scene13[3]), Create(Px1y1_copy_scene13),
                 Transform(Px1y1_scene13, Text_3_scene13[4]), Write(Text_3_scene13[5]), Write(Text_3_scene13[6]),
                 run_time=2)
