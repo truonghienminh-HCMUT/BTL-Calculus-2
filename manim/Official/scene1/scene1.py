f8c5ce8a387d7bc16641caa88bee99c048045226
Trần Ngọc Phương Mai <mai.tranngocphuongmai2452720@hcmut.edu.vn>
2025-05-27 15:39:24 +0700

    scene1

---
+++ manim/Official/scene1/scene1.py
@@ -0,0 +1,39 @@
+from manim import *
+
+config.media_width = "100%"
+VIETNAMESE_TEMPLATE = TexTemplate(
+    preamble=r"""
+    \usepackage[utf8]{inputenc}
+    \usepackage[T5]{fontenc}
+    \usepackage{amsmath}
+    \usepackage{amssymb}
+    \usepackage{lmodern}
+    \usepackage{graphicx}
+    \usepackage{tikz}
+    """
+)
+# Set the default TeX template
+config.tex_template = VIETNAMESE_TEMPLATE
+
+config.frame_rate = 60
+
+class ChiaNho(MovingCameraScene):
+    def construct(self):
+        self.camera.frame.save_state()
+         
+        #SCENE 1: GIỚI THIỆU CHỦ DỀ VÀ TÊN THÀNH VIÊN
+        Text_ChuDe_scene1 = Tex(r"\textbf{CHỦ ĐỀ 7}", font_size=45).shift(UP* 1)
+        Text_TenChuDe_scene1 = Tex(r"\textbf{TÍCH PHÂN KÉP TRONG TỌA ĐỘ CỰC}", font_size=55)
+        Text_TenNhom_scene1= Tex(r"\textbf{NHÓM NM02 - LỚP CN01}", font_size=45).shift(DOWN * 1)
+
+        self.play(Write(Text_ChuDe_scene1), Write(Text_TenChuDe_scene1), run_time=2)
+        self.play(Write(Text_TenNhom_scene1), run_time=2)
+        self.wait(2)
+        self.play(FadeOut(Text_ChuDe_scene1), FadeOut(Text_TenChuDe_scene1), FadeOut(Text_TenNhom_scene1), run_time=1)
+
+        def update_curve(mob):
+            mob.move_to(moving_dot.get_center())
+
+        self.camera.frame.remove_updater(update_curve)
+
+        self.play(Restore(self.camera.frame))
