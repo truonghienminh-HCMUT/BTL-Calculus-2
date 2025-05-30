23df42796786a0865971fe7466678b4dbe33e046
Trương Hiển Minh <minh.truonghien@hcmut.edu.vn>
2025-05-30 22:41:06 +0700

    Upload files

--- manim/Official/scene14-23/scene14-23.py
+++ manim/Official/scene14-23/scene14-23.py
@@ -17,7 +17,7 @@ config.tex_template = VIETNAMESE_TEMPLATE
 
 config.frame_rate = 60
 
-class ChiaNho(MovingCameraScene):
+class scene14_23(MovingCameraScene):
     def construct(self):
         self.camera.frame.save_state()
 
@@ -1072,6 +1072,14 @@ class ChiaNho(MovingCameraScene):
             r"\textbf{4. GIẢI QUYẾT VÍ DỤ THỰC TIỄN}",
             font_size=50
         )
+        Atiso = ImageMobject("Atiso.jpg")
+        Atiso.scale(1).shift(LEFT  * 3.8)
+        DeViDu = Tex(r"\textbf{Anh A mô phỏng được tòa nhà bằng một hình cầu}", font_size=30).shift(UP * 2 + RIGHT * 3).set_color(YELLOW)
+        DeViDu_2 = Tex(r"\textbf{và một hình paraboloid tròn}", font_size=30).shift(UP * 1.5 + RIGHT * 2.5).set_color(YELLOW)
+        ChieuDai = Tex(r"12m chiều cao (h)", font_size=30).shift(UP * 0.6 + RIGHT * 2.7)
+        BanKinh = Tex(r"4m bán kính của đường tròn đáy (r)", font_size=30).shift( RIGHT * 2.7 + DOWN * 0.2)
+        KhoangCach = Tex(r"3m khoảng cách từ mặt đất đến tâm hình cầu (d)", font_size=30).shift(DOWN * 1 + RIGHT * 2.7)
+
         Text_1_scene23.set_color_by_gradient(color_03ffff, color_cc00ff)
         ViDu_1_scene23 = Tex(r"Áp dụng công thức, ta tính được:", font_size=45).shift(UP * 1)
         ViDu_2_scene23 = Tex(r"$R^2=r^2+d^2=4^2+3^2=5^2$", font_size=45)
@@ -1081,7 +1089,7 @@ class ChiaNho(MovingCameraScene):
         ViDu_6_scene23 = Tex(r"$f(x,y)=z=\sqrt{5^2-x^2-y^2}$", font_size=40).shift(UP * 3)
         ViDu_7_scene23 = Tex(r"Ta có khoảng cách từ mặt đáy đến tâm hình cầu là $d = 3m$. Do hình cầu mô phỏng nằm bên dưới mặt Oz nên mặt đáy phải nằm bên trên mặt $z = -3$ và mặt phẳng $z = -3$ cũng chính là mặt đất thực tế.",
                             font_size=35)
-        ViDu_8_scene23 = Tex(r"Tiếp theo là phần đỉnh, từ quan sát anh thấy rằng nó có dạng khá giống hình paraboloid tròn (dạng đặc biệt của elliptic paraboloid) có phương trình:",
+        ViDu_8_scene23 = Tex(r"Tiếp theo là phần đỉnh, từ quan sát ta thấy rằng nó có dạng khá giống hình paraboloid tròn (dạng đặc biệt của elliptic paraboloid) có phương trình:",
                             font_size=35)
         ViDu_9_scene23 = MathTex(r"f(x,y)=z=a(x^2+y^2)+b", font_size=40).shift(DOWN * 0.5)
         ViDu_10_scene23 = Tex(r"Sau khi dùng các số liệu thực tế để tính toán, ta có:", font_size=35).shift(UP * 1)
@@ -1097,8 +1105,8 @@ class ChiaNho(MovingCameraScene):
         ViDu_20_scene23 = MathTex(
             r"\left\{ \begin{array}{l}"
             r"4 \leq r \leq 5 \\"
-            r"0 \leq \varphi \leq 2\pi"
-            r"\end{array} \right.", 
+            r"0 \leq \varphi \leq 2 \pi"
+            r"\end{array} \right." , 
             font_size=40
         ).shift(DOWN * 0.3)
         ViDu_21_scene23 = MathTex(r"S_c = \int_0^{2\pi} d\varphi \int_4^5 r\,dr\, \sqrt{\frac{25}{25 - r^2}}", font_size=40).shift(UP * 0.8)
@@ -1112,7 +1120,7 @@ class ChiaNho(MovingCameraScene):
         ViDu_30_scene23 = MathTex(
             r"\left\{ \begin{array}{l}"
             r"0 \leq r \leq 4 \\"
-            r"0 \leq \varphi \leq 2\pi"
+            r"0 \leq \varphi \leq 2 \pi"
             r"\end{array} \right.", 
             font_size=40
         ).shift(UP * 1)
@@ -1132,18 +1140,22 @@ class ChiaNho(MovingCameraScene):
         ViDu_36_scene23 = MathTex(
             "=", "\\frac{32\\pi}{27} \\left( 10\\sqrt{10} - 1 \\right)", font_size=40
         ).shift(DOWN * 2.7)
-        ViDu_37_scene23 = Tex(r"Anh cho biết anh sẽ dùng kính để làm tòa nhà, vậy diện tích kính cần sử dụng là:", font_size=35).shift(UP * 1.5)
+        ViDu_37_scene23 = Tex(r"Anh A cho biết anh sẽ dùng kính để làm tòa nhà, vậy diện tích kính cần sử dụng là:", font_size=35).shift(UP * 1.5)
         ViDu_38_scene23 = MathTex("\\frac{32\\pi}{27}(10\\sqrt{10} - 1) + 60\\pi\\,(m^2)", font_size=40)
-        ViDu_39_scene23 = Tex(r"Anh sử dụng kính cường lực $10 mm$ Việt Nhật $480.000$ VNĐ$/m^2$, vậy số tiền cần sử dụng là:", font_size=35).shift(UP * 0.8)
+        ViDu_39_scene23 = Tex(r"Anh A sử dụng kính cường lực $10 mm$ Việt Nhật $480.000$ VNĐ$/m^2$, vậy số tiền cần sử dụng là:", font_size=35).shift(UP * 0.8)
         ViDu_40_scene23 = MathTex(
             "\\left[ \\frac{32\\pi}{27}(10\\sqrt{10} - 1) + 60\\pi \\right]"
-            " \\times 480\\,000 \\approx 145\\,154\\,000 \\ (\\text{VNĐ})", font_size=40
+            " \\times 480\\,000 \\approx 145\\,207\\,000 \\ (\\text{VNĐ})", font_size=40
         ).shift(DOWN * 0.7)
 
 
         self.play(Write(Text_1_scene23), run_time=2)
         self.wait(1)
         self.play(FadeOut(Text_1_scene23))
+        self.play(SpinInFromNothing(Atiso), run_time=2)
+        self.play(Write(DeViDu), Write(DeViDu_2), Write(ChieuDai), Write(BanKinh), Write(KhoangCach), run_time=2)
+        self.wait(1)
+        self.remove(DeViDu, DeViDu_2, ChieuDai, BanKinh, KhoangCach, Atiso)
         self.play(Write(ViDu_1_scene23), run_time=1)
         self.play(Write(ViDu_2_scene23), run_time=1)
         self.play(Write(ViDu_3_scene23), run_time=1)
@@ -1171,6 +1183,8 @@ class ChiaNho(MovingCameraScene):
         self.play(Write(ViDu_10_scene23), run_time=2)
         self.play(Transform(ViDu_9_scene23, ViDu_11_scene23), run_time=1)
         self.wait(1)
+        self.play(ViDu_19_scene23.animate.move_to(UP * 3), run_time=2)
+        self.wait(3)
         self.remove(ViDu_9_scene23, ViDu_10_scene23)
         self.wait(1)
         self.play(Write(ViDu_12_scene23), run_time=2)
@@ -1217,10 +1231,6 @@ class ChiaNho(MovingCameraScene):
         self.play(Circumscribe(ViDu_40_scene23))
         self.wait(1)
         self.play(FadeOut(ViDu_39_scene23), FadeOut(ViDu_40_scene23))
-
-        
-
-
 
         def update_curve(mob):
             mob.move_to(moving_dot.get_center())
